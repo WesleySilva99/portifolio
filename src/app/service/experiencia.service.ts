bfb65ba66a5bcb9aa6faa9027c9aa31e8c2a6109
WesleySilva99 <wesleyacad001@gmail.com>
2022-03-04 00:50:27 -0300

    test trazendo projeto para a raiz

---
+++ src/app/service/experiencia.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Experience} from "../model/Experience";
+import {Observable} from "rxjs";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ExperienciaService {
+
+   readonly apiURL: string = 'https://portifolio-wesley-silva.herokuapp.com/api/v1/experiencia';
+  //readonly apiURL: string = 'http://localhost:8080/api/v1/experiencia';
+
+  constructor(private http: HttpClient) {
+
+  }
+
+  listaTodos():Observable<Experience[]>{
+
+    return this.http.get<Experience[]>(`${ this.apiURL }`);
+
+  }
+
+}
